ebb079b60e912d1511af5bddb7712b5075471ec1
Michal Bryxí <michal.bryxi@gmail.com>
2015-10-09 20:14:35 +0200

    Static pages
    - Added homepage
    - Tweaked URL creation hack a bit, so that redirects within the app are cleaner
    - Added some descriptive text into the app

--- app/index/route.js
+++ app/index/route.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   beforeModel: function() {
-    var url = this.store.createRecord('url', {value: 'http://localhost:4200/#/posts/11/edit?hideName=true&selected=body'});
-    this.transitionTo('url', url.id);
+    this.transitionTo('url');
   }
 });
